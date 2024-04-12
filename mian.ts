a17956f7d2e3ca2e5aa0f35e6572e256e900f1e0
studentmeomeogithub <arsalanameomeo9@gmail.com>
2024-04-12 21:21:46 +0500

    first commit

---
+++ mian.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+
+let todos = [];
+
+let condition = true
+
+while(condition){
+
+
+
+let addTask = await inquirer.prompt([{
+    name:"todo",
+    type:"input",
+    message:"what do you want to add in your todos ?"
+},
+{
+    name:"addMore",
+    type:"confirm",
+    message:"Do you want add more ?",
+    default:"false"
+}
+]);
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos)
+}
